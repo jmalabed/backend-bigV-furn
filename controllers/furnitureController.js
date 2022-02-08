2ea51a1052b016e20c2f9ef5ec0749c33717fe08
jmalabed <jmalabed27@gmail.com>
2022-02-08 14:35:50 -0800

    updated route to fix return value

--- controllers/furnitureController.js
+++ controllers/furnitureController.js
@@ -25,7 +25,7 @@ router.get("/:id", async (req, res, next) => {
   try {
     const id = req.params.id;
     const foundFurn = await Furniture.findById(id);
-    res.status(200).json({ foundFurn });
+    res.status(200).json(foundFurn);
   } catch (err) {
     res.status(400).json({ err: err.message });
   }
